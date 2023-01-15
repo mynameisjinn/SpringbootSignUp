b3f148499fdc053ee7f0f47bd5f04270b84cff37
hyejin <obndo8539@naver.com>
2023-01-15 16:33:16 +0900

    DTO에 Validation , BindingResult

--- src/main/java/com/study/springboothyejin/web/controller/account/AccountApiController.java
+++ src/main/java/com/study/springboothyejin/web/controller/account/AccountApiController.java
@@ -5,29 +5,46 @@ import com.study.springboothyejin.web.dto.CMRespDto;
 import com.study.springboothyejin.web.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
-@Validated
+//@Validated
 @RequestMapping("/api/account")
 public class AccountApiController {
     @Autowired
     private UserService userService;
 
     @GetMapping("/username")
-    public ResponseEntity<?> duplicateUsername(@NotBlank(message = "사용자 이름을 입력하세요.") String username) {
+    public ResponseEntity<?> duplicateUsername(@Pattern(regexp = "^[a-zA-Z\\d]{5,20}$"
+            ,message = "사용자 이름은 영문, 숫자조합이어야하며 <br>5자 이상 20자 이하로 작성하세요") String username) {
 //        System.out.println(username);
         userService.duplicateUsername(username);
         return ResponseEntity.ok().body(new CMRespDto<>("가입 가능한 사용자이름", true));
     }
 
     @PostMapping("/user")
-    public ResponseEntity<?> register(@RequestBody UserDto userDto) {
-        System.out.println(userDto);
+    public ResponseEntity<?> register(@RequestBody @Valid UserDto userDto,
+                                      BindingResult bindingResult) {
+//        System.out.println(userDto);
+//        System.out.println(bindingResult.getFieldErrors());
+        if(bindingResult.hasErrors()) {
+            Map<String,String> errorMap = new HashMap<String,String>();
+            bindingResult.getFieldErrors().forEach(error -> {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            });
+            errorMap.forEach((k, v) ->{
+                System.out.println(k+" : "+v);
+            });
+        }
         return ResponseEntity
                 .created(URI.create("/account/login"))
                 .body(new CMRespDto<>("회원가입 완료", null));
--- src/main/java/com/study/springboothyejin/web/dto/UserDto.java
+++ src/main/java/com/study/springboothyejin/web/dto/UserDto.java
@@ -5,7 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Builder
 @NoArgsConstructor
@@ -15,8 +17,16 @@ public class UserDto {
     private int userId;
 
     @NotBlank
+    @Pattern(regexp = "^[a-zA-Z\\d]{5,20}$",
+            message = "사용자 이름은 영문, 숫자조합이어야하며 5자 이상 20자 이하로 작성하세요")
     private String username;
+
     private String password;
+
+    @Pattern(regexp = "^[가-힇]{2,5}$",message = "이름은 한글만 작성 가능하며 2자 이상 5자 이하만 입력가능합니다")
     private String name;
+
+    @NotBlank(message = "이메일을 입력해주세요")
+    @Email
     private String email;
 }
